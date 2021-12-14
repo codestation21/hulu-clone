eebd0b1460e87634704cd25f40f2a772b6ccaee5
Siam Ahnaf <78312010+siamahnaf198@users.noreply.github.com>
2021-12-15 00:49:26 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,4 +5,9 @@ module.exports = {
   images: {
     domains: ['image.tmdb.org'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
